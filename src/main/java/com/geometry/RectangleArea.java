7062ac010250ab87e3f7e49cef88d1449c2ca744
naveenmr2001 <naveen.mr@m2pfintech.com>
2023-03-29 15:52:16 +0530

    Requirement 1: Add. I have added the logic and testcase to Rectangle Area

--- src/main/java/com/geometry/RectangleArea.java
+++ src/main/java/com/geometry/RectangleArea.java
@@ -1,7 +1,19 @@
 package com.geometry;
 
-public class RectangleArea {
-    public int areaOfRectangle(int lengthOfSide, int breathOfSide){
-        System.out.println("Hello world");
+
+// A Class that represents use-defined exception
+
+class ExceptionToHandleTheNegativeNumber extends Exception {
+    public ExceptionToHandleTheNegativeNumber(String s)
+    {
+        super(s);
+    }
+}
+public class RectangleArea{
+    public int areaOfRectangle(int lengthOfSide, int breathOfSide) throws ExceptionToHandleTheNegativeNumber {
+        if(lengthOfSide<0 || breathOfSide<0){
+            throw new ExceptionToHandleTheNegativeNumber("It should be greater than zero");
+        }
+        return lengthOfSide*breathOfSide;
     }
 }
--- src/test/java/com/geometry/RectangleTest.java
+++ src/test/java/com/geometry/RectangleTest.java
@@ -3,14 +3,33 @@ package com.geometry;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import java.awt.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RectangleTest {
     RectangleArea rectangleObject = new RectangleArea();
-    @Test
-    void toCheckTheRectangleArea(){
 
+    @Nested
+    class RectangleAreaTest{
+        @Test
+        void toCheckTheRectangleAreaForInputThreeAndFour() throws ExceptionToHandleTheNegativeNumber {
+
+            int exceptedValue = 12;
+
+            int actualValue = rectangleObject.areaOfRectangle(3,4);
+
+            assertEquals(exceptedValue,actualValue);
+
+        }
+
+        @Test
+        void toCheckTheExceptionForInputNegativeThreeAndFour(){
+
+            assertThrows(ExceptionToHandleTheNegativeNumber.class,()->rectangleObject.areaOfRectangle(-3,4));
+
+        }
     }
+
 
 
 }
